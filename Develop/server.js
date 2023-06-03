9253d8d7f7e6b6ecd56b2838f653f8bf9adce4ef
Leo Liu <aleoliu96@gmail.com>
2023-06-03 00:28:30 -0400

    Added more to server,js, making sure it can start local host

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,16 @@ const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
