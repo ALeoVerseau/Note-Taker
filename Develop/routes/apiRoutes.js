ca5e1c46da09b0f276b7495a5a8617cba1eda0a2
Leo Liu <aleoliu96@gmail.com>
2023-06-07 20:31:18 -0400

    added post route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,10 +2,23 @@ const fs = require("fs");
 const uuid = require("uuid");
 const router = require("express").Router();
 
-
+// get route for notes
 router.get("/api/notes", (req, res) => {
     const dbData = JSON.parse(fs.readFileSync("./db/db.json"));
     res.jason(JSON.parse(dbData));
+});
+
+// post req for notes
+router.post("/notes", (req, res) => {
+    const dbData = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNote = {
+        titie: req.body.title,
+        text: req.body.text,
+        id: uuid.v4(),
+    };
+    dbData.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbData));
+    res.json(dbData)
 });
 
 
--- Develop/routes/indexRoutes.js
+++ Develop/routes/indexRoutes.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 
 // get request for index.html
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/pulblic/index.html"));
+    res.sendFile(path.join(__dirname, "../pulblic/index.html"));
 });
 
 // get request for notes.html
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 module.exports = router
