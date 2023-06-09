5accd3347d62526ba06f79fcda4e94f826b3ea50
Leo Liu <aleoliu96@gmail.com>
2023-06-08 21:25:19 -0400

    fixing save errors

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,16 +3,16 @@ const uuid = require("uuid");
 const router = require("express").Router();
 
 // get route for notes
-router.get("/api/notes", (req, res) => {
+router.get("/notes", (req, res) => {
     const dbData = JSON.parse(fs.readFileSync("./db/db.json"));
-    res.jason(JSON.parse(dbData));
+    res.json(dbData);
 });
 
 // post req for notes
 router.post("/notes", (req, res) => {
     const dbData = JSON.parse(fs.readFileSync("./db/db.json"));
     const newNote = {
-        titie: req.body.title,
+        title: req.body.title,
         text: req.body.text,
         id: uuid.v4(),
     };
